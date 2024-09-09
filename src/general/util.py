8c8208e31bc4a88ffcc3348a51f66e3503a9e79c
Volchonok <82080790+V0ch0n0k@users.noreply.github.com>
2024-09-09 21:48:09 +0300

    change: path accessing to demo_data files in run_demo

--- src/general/util.py
+++ src/general/util.py
@@ -6,7 +6,6 @@ __all__ = ["load_data_from_json_by_path", "clean_newlines"]
 
 
 def load_data_from_json_by_path(file_path: str) -> Dict:
-    print(f"file_path={file_path}")
     with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
 
--- src/run_demo.py
+++ src/run_demo.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -5,32 +7,37 @@ from config.settings import get_settings
 from general.util import load_data_from_json_by_path
 from src.general.enums import Language
 from src.general.models import BaseModel
-from src.images.models import DType
 from src.partners import PartnerService
 from src.team_members import TeamMemberService
 
 SOURCE_LANGUAGE_INDEX = Language.EN.index
 
+IMAGE_PATH = f"{get_settings().BASE_RESOURCE_PATH}/demo_data"
+
 
 def main():
-    print(f"DTypes={DType.values()}")
     data = load_data_from_json_by_path(f"{get_settings().BASE_RESOURCE_PATH}/demo_data/data.json")
 
     partner_service = PartnerService(session)
     team_member_service = TeamMemberService(session)
 
+    data["partner"]["logo_path"] = os.path.join(IMAGE_PATH, os.path.basename(data["partner"]["logo_path"]))
     partner = partner_service.create_partner(source_language=SOURCE_LANGUAGE_INDEX, data=data["partner"])
     session.flush()
+
     partner_localizations = [loc for loc in data["partner"]["localizations"]]
     partner_service.add_localizations(partner.id, partner_localizations)
 
     for member in data["team_member"]:
+        member["photo_path"] = os.path.join(IMAGE_PATH, os.path.basename(member["photo_path"]))
         team_member = team_member_service.create_team_member(
             source_language=SOURCE_LANGUAGE_INDEX, partner_id=partner.id, data=member
         )
         session.flush()
+
         team_member_localizations = [loc for loc in member["localizations"]]
         team_member_service.add_localizations(team_member.id, team_member_localizations)
+
     session.commit()
 
 
